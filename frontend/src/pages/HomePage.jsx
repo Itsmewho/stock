ecee48d056311bd09c58d2ecc3d07704930de990
itsmewho <jordy_mt@live.nl>
2025-11-05 05:14:43 +0100

    first commit check

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ function HomePage() {
         <div className={style.gridMain}>
           <div className={style.subGrid}>
             <div className={style.panel}>stock chart</div>
-            <div className={style.panel}>fundamentals</div>
+            <div className={style.panel}>fundamentals poep</div>
           </div>
         </div>
         <div className={style.gridSubMenu}>submenu</div>
